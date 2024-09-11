a132172724eee7af07f8ae09557bf88296e59ca3
khalilaliev <khalilaliev0@gmail.com>
2024-09-11 13:20:36 +0200

    fix footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,20 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import About from "./pages/About";
 import Home from "./pages/Home";
-// import About from "./pages/About";
 
 function App() {
   return (
     <>
-      <Header />
-      <main className="max-w-2xl my-0 mx-auto mt-5 container pt-20 h-80vh-screen">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </main>
-      <Footer />
+      <div className="flex flex-col min-h-screen">
+        <Header />
+        <main className="flex-grow max-w-2xl mx-auto mt-5 pt-20 container">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
